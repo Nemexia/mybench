b0c355255a75772f4ac7f3953c99b6b83b276cd7
Nemexia <amirhossein.rahmani55@gmail.com>
2025-03-12 18:50:58 +0330

    Refactor files

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include "mybench.h"
 #include <thread>
 
-void f1() {
-	std::this_thread::sleep_for(std::chrono::milliseconds(50));
-}
+void f1() { std::this_thread::sleep_for(std::chrono::milliseconds(50)); }
 
 int main() {
-	Benchmark b;
-	b.run(f1, 100, 100);
-	b.print();
-	return 0;
+  Benchmark b;
+  b.run(f1, 100, 100);
+  b.print();
+  return 0;
 }
--- mybench.h
+++ mybench.h
@@ -4,31 +4,31 @@
 
 class Benchmark {
 private:
-    long long rounds{ 0 };
-    double mean{ 0.0 };
-    double std{ 0.0 };
-    double min{ 0.0 };
-    double max{ 0.0 };
-    double sum_sqdiff{ 0.0 };
+  long long rounds{0};
+  double mean{0.0};
+  double std{0.0};
+  double min{0.0};
+  double max{0.0};
+  double sum_sqdiff{0.0};
 
-    void update_stats(double duration);
+  void update_stats(double duration);
 
 public:
-    template <typename Func>
-    void run(Func func, int rounds_todo = 100, int cycles_todo = 1) {
-        for (int r{ 0 }; r < rounds_todo; ++r) {
-            auto start = std::chrono::high_resolution_clock::now();
-            for (int c{ 0 }; c < cycles_todo; ++c) {
-                func();
-            }
-            auto end = std::chrono::high_resolution_clock::now();
-            auto duration =
-                std::chrono::duration_cast<std::chrono::microseconds>(end - start)
-                .count();
-            update_stats(static_cast<double>(duration) / cycles_todo);
-            print();
-        }
+  template <typename Func>
+  void run(Func func, int rounds_todo = 100, int cycles_todo = 1) {
+    for (int r{0}; r < rounds_todo; ++r) {
+      auto start = std::chrono::high_resolution_clock::now();
+      for (int c{0}; c < cycles_todo; ++c) {
+        func();
+      }
+      auto end = std::chrono::high_resolution_clock::now();
+      auto duration =
+          std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+              .count();
+      update_stats(static_cast<double>(duration) / cycles_todo);
+      print();
     }
+  }
 
-    void print() const;
+  void print() const;
 };
