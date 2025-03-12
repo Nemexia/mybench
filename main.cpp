2f30186f4de051cb97b416360c26b9bb41ac52c0
Nemexia <amirhossein.rahmani55@gmail.com>
2025-03-12 21:43:37 +0330

    constexpr test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include "mybench.h"
-#include <thread>
+#include <iostream>
 
-void f1() { std::this_thread::sleep_for(std::chrono::milliseconds(50)); }
+void f1() {
+  constexpr int size{1000};
+  constexpr int cycle{100};
+  volatile int data[size];
+  for (auto &d : data)
+    d = 0;
+
+  data[0] = 1;
+  for (int c{0}; c < cycle; ++c)
+    for (int i{0}; i < size; ++i)
+      data[i] = data[i - 1] + 1;
+
+  //  std::cout << data[0] << '.';
+}
 
 int main() {
   Benchmark b;
-  b.run(f1, 100, 100);
+  b.run(f1, 25, 1000);
+  b.run(f1, 25, 1000);
   return 0;
 }
